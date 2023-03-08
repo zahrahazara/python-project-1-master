0da20a831e2d9ae0f1727f85dd97cfbef41f7d45
zahrahazara <zahra.hazara3@gmail.com>
2023-03-08 13:20:00 +0200

    porject_1_uusi_säännööt

--- python-project-1-master/rule.py
+++ python-project-1-master/rule.py
@@ -2,7 +2,7 @@
 text = "Yhteensä on 31 lentokentää, joka voit lentää vapaasti etsimään timantti.\n\
     Saat 500Km, millä voit alka matkaa etsimään timantti.\n\
     Jokaisessa lentokentällä, ilmoitetaan etäisyys timantille sijainnista. \n\
-    Jokaisen lento jälkeen tulee ehto haluatko voitaa lisää km.\n\
+    Jokaisen lennon jälkeen tulee ehto haluatko voitaa lisää km.\n\
     Saat 200km kun vastaus oikein jokaisesta laskutehtävistä.\n\
     Väärä vastaus minustetaan 50km.\n\
     Jos kilometrimäärä loppuu kesken matkalla, peli loppuu tai lentänyt yli 10 kertaa."
